efc3d22e179fa17e1514c49a6d2a2cf0ac219768
serii burduja <seriiburduja@gmail.com>
2024-09-16 12:45:38 +0300

    feat: check for prime number

--- lessons/f08_encodeWord.py
+++ lessons/f08_encodeWord.py
@@ -1,9 +1,18 @@
 import math
 def f08_encodeWord():
-    test_w  = int(input('Width: '))
-    test_h  = int(input('Height: '))
-    coverage = 5
-    paintCalc(width=test_w,height=test_h,can=coverage)
+    # test_w  = int(input('Width: '))
+    # test_h  = int(input('Height: '))
+    # coverage = 5
+    # paintCalc(width=test_w,height=test_h,can=coverage)
+    n = int(input('Check this number: '))
+    primeChecker(number=n)
+
+def primeChecker(number):
+    for i in range(2, number):
+        if number % i == 0:
+            print("It's not a prime number")
+            return
+    print("It's a prime number")
 
 
 def paintCalc(width, height, can = 5):
