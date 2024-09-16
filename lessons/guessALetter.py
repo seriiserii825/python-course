c6b5dc354d9efabedae6492d9377d8cb9620ea42
serii burduja <seriiburduja@gmail.com>
2024-09-16 09:07:12 +0300

    feat: guessALetter dash array

--- lessons/guessALetter.py
+++ lessons/guessALetter.py
@@ -3,7 +3,25 @@ def guessALetter():
     words = ['new', 'next', 'second']; 
     choose_word = random.choice(words);
     print(choose_word)
+
+    empty_array = []
+
+    choose_word_length = len(choose_word);
+    for _ in range(0, choose_word_length):
+        empty_array.append("_")
+    # print(empty_array)
+
     guess = input('Choose a letter: ').lower()
-    print(guess)
+    # print(guess)
+
+    for i, letter in enumerate(choose_word):
+        # print(i)
+        if letter == guess:
+            empty_array[i] = letter
+        else:
+            continue
+
+    print(empty_array)
+
 
 guessALetter()
