105f316aa69a2aba823aa503ba0085df19b62cff
serii burduja <seriiburduja@gmail.com>
2024-09-18 17:48:06 +0300

    feat: encrypt first step

--- lessons/f08_encodeWord.py
+++ lessons/f08_encodeWord.py
@@ -1,10 +1,19 @@
 def f08_encodeWord():
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    direction = input("Type 'encode', or 'decode':") 
+    # direction = input("Type 'encode', or 'decode':") 
     text = input('Type your message:\n').lower()
     shift = int(input("Type the shif number:\n"))
 
+    encrypt(text, shift, alphabet)
 
-def encrypt():
-    print("encrypting")
+def encrypt(plain_text, shift_amount, alphabet):
+    cipher_text = "" 
+    for letter in plain_text:
+       position = alphabet.index(letter)
+       new_position = position + shift_amount
+       new_letter = alphabet[new_position]
+       cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
 
+
+f08_encodeWord()
