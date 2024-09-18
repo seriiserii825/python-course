3d6c08c54e899ef80e956db33a5546b2c0d5159f
serii burduja <seriiburduja@gmail.com>
2024-09-18 18:13:27 +0300

    feat: resole bug with shift amount

--- lessons/f08_encodeWord.py
+++ lessons/f08_encodeWord.py
@@ -4,15 +4,19 @@ def f08_encodeWord():
     text = input('Type your message:\n').lower()
     shift = int(input("Type the shif number:\n"))
 
-    encrypt(text, shift, alphabet)
+    encrypt(plain_text=text, shift_amount=shift, alphabet=alphabet)
 
 def encrypt(plain_text, shift_amount, alphabet):
     cipher_text = "" 
+    alphabet_length = len(alphabet)
     for letter in plain_text:
-       position = alphabet.index(letter)
-       new_position = position + shift_amount
-       new_letter = alphabet[new_position]
-       cipher_text += new_letter
+        position = alphabet.index(letter)
+        if position + shift_amount >= alphabet_length:
+            new_position = position + shift_amount - alphabet_length
+        else:
+            new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
     print(f"The encoded text is {cipher_text}")
 
 
