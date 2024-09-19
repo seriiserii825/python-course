02f4218c5645a442d4ed854c8728f912baa594da
serii burduja <seriiburduja@gmail.com>
2024-09-19 07:44:51 +0300

    feat: check for number symbol or space

--- lessons/f08_encodeWord.py
+++ lessons/f08_encodeWord.py
@@ -14,19 +14,22 @@ def caesar(text, shift_amount, alphabet, direction):
     final_text = "" 
     alphabet_length = len(alphabet)
     for letter in text:
-        position = alphabet.index(letter)
-        if (direction == 'encode'):
-            if position + shift_amount >= alphabet_length:
-                new_position = position + shift_amount - alphabet_length
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            if (direction == 'encode'):
+                if position + shift_amount >= alphabet_length:
+                    new_position = position + shift_amount - alphabet_length
+                else:
+                    new_position = position + shift_amount
             else:
-                new_position = position + shift_amount
+                if position - shift_amount < 0:
+                    new_position = alphabet_length + (position - shift_amount)
+                else:
+                    new_position = position - shift_amount
+            new_letter = alphabet[new_position]
+            final_text += new_letter
         else:
-            if position - shift_amount < 0:
-                new_position = alphabet_length + (position - shift_amount)
-            else:
-                new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        final_text += new_letter
+            final_text += letter
     print(f"The [blue]{direction}d[/] text is [green]{final_text}[/]")
 
 f08_encodeWord()
