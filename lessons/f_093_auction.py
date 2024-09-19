57c64fc140ce39cf6fcd603b3e5b09d2edc273ea
serii burduja <seriiburduja@gmail.com>
2024-09-19 12:51:08 +0300

    feat: f93 auction

--- lessons/f_093_auction.py
+++ lessons/f_093_auction.py
@@ -1,5 +1,30 @@
+from replit import clear
 from logos.auction_logo import logo
-def f_093_auction():
-    print(logo)
+print(logo)
+users = []
 
-f_093_auction()
+def createUser():
+    name = input("What is your name?: ")
+    bid = int(input("What's your bid?: $"))
+    user = { "name": name, "bid": bid }
+    users.append(user)
+
+def calcMax():
+    max = 0
+    name = ''
+    for item in users:
+        if item['bid'] > max:
+            max = item['bid']
+            name = item['name']
+    print(f"Won {name}, with bid: {max}")
+
+
+while True:
+    createUser()
+    more = input("Are there any other bidders? Type 'yes' or 'no':")
+    if more == 'no':
+        print(users)
+        calcMax()
+        break
+    clear()
+
