0916ce0b65b42543cf101363c358036a24eae57c
serii burduja <seriiburduja@gmail.com>
2024-09-19 07:07:36 +0300

    feat: finished decode

--- lessons/f08_encodeWord.py
+++ lessons/f08_encodeWord.py
@@ -1,10 +1,13 @@
 def f08_encodeWord():
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    # direction = input("Type 'encode', or 'decode':") 
+    direction = input("Type 'encode', or 'decode':") 
     text = input('Type your message:\n').lower()
     shift = int(input("Type the shif number:\n"))
 
-    encrypt(plain_text=text, shift_amount=shift, alphabet=alphabet)
+    if direction == 'encode':
+        encrypt(plain_text=text, shift_amount=shift, alphabet=alphabet)
+    else:
+        decrypt(cipher_text=text, shift_amount=shift, alphabet=alphabet)
 
 def encrypt(plain_text, shift_amount, alphabet):
     cipher_text = "" 
@@ -18,6 +21,19 @@ def encrypt(plain_text, shift_amount, alphabet):
         new_letter = alphabet[new_position]
         cipher_text += new_letter
     print(f"The encoded text is {cipher_text}")
+
+def decrypt(cipher_text, shift_amount, alphabet):
+    plain_text = "" 
+    alphabet_length = len(alphabet)
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        if position - shift_amount < 0:
+            new_position = alphabet_length + (position - shift_amount)
+        else:
+            new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        plain_text += new_letter
+    print(f"The encoded text is {plain_text}")
 
 
 f08_encodeWord()
