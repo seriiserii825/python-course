8f22e8b933d7a319f3e7634bcb83a9cda0f1ed29
serii burduja <seriiburduja@gmail.com>
2024-09-19 07:21:15 +0300

    feat: finished decode

--- lessons/f08_encodeWord.py
+++ lessons/f08_encodeWord.py
@@ -1,39 +1,30 @@
+from rich import print
+from rich.console import Console
+console = Console()
 def f08_encodeWord():
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    direction = input("Type 'encode', or 'decode':") 
-    text = input('Type your message:\n').lower()
-    shift = int(input("Type the shif number:\n"))
+    direction = console.input("Type [green]'encode'[/], or [blue]'decode'[/]:") 
+    text =console.input('Type your [yellow]message[/]:\n').lower()
+    shift = int(console.input("Type the [blue]shif[/] number:\n"))
+    caesar(text=text, shift_amount=shift, alphabet=alphabet, direction=direction)
 
-    if direction == 'encode':
-        encrypt(plain_text=text, shift_amount=shift, alphabet=alphabet)
-    else:
-        decrypt(cipher_text=text, shift_amount=shift, alphabet=alphabet)
-
-def encrypt(plain_text, shift_amount, alphabet):
-    cipher_text = "" 
+def caesar(text, shift_amount, alphabet, direction):
+    final_text = "" 
     alphabet_length = len(alphabet)
-    for letter in plain_text:
+    for letter in text:
         position = alphabet.index(letter)
-        if position + shift_amount >= alphabet_length:
-            new_position = position + shift_amount - alphabet_length
+        if (direction == 'encode'):
+            if position + shift_amount >= alphabet_length:
+                new_position = position + shift_amount - alphabet_length
+            else:
+                new_position = position + shift_amount
         else:
-            new_position = position + shift_amount
+            if position - shift_amount < 0:
+                new_position = alphabet_length + (position - shift_amount)
+            else:
+                new_position = position - shift_amount
         new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
-
-def decrypt(cipher_text, shift_amount, alphabet):
-    plain_text = "" 
-    alphabet_length = len(alphabet)
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        if position - shift_amount < 0:
-            new_position = alphabet_length + (position - shift_amount)
-        else:
-            new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        plain_text += new_letter
-    print(f"The encoded text is {plain_text}")
-
+        final_text += new_letter
+    print(f"The [blue]{direction}d[/] text is [green]{final_text}[/]")
 
 f08_encodeWord()
