17d13eec37feb1330fd944dac6f2f5e96f4e9c92
serii burduja <seriiburduja@gmail.com>
2024-09-23 17:56:54 +0300

    feat: calc start

---
+++ lessons/f_101_calculator.py
@@ -0,0 +1,18 @@
+def f_101_calculator():
+    pass
+
+#add
+def add(a, b):
+    return a + b
+
+#subtract
+def subtract(a, b):
+    return a - b
+
+#multiply
+def multiply(a, b):
+    return a * b
+
+#divide
+def divide(a, b):
+    return a / b
