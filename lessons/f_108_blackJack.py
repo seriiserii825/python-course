9c14c208785e77f6a9c881d1a82118cb76e359e1
serii burduja <seriiburduja@gmail.com>
2024-09-24 09:02:39 +0300

    feat: 109 start

--- lessons/f_108_blackJack.py
+++ lessons/f_108_blackJack.py
@@ -5,6 +5,25 @@
 ## Use the following list as the deck of cards:
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
+
+#Difficulty Normal @: Use all Hints below to complete the project.
+#Difficulty Hard @: Use only Hints 1, 2, 3 to complete the project.
+#Difficulty Extra Hard @: Only use Hints 1 & 2 to complete the project.
+#Difficulty Expert @: Only use Hint 1 to complete the project. 56
+
+#Hint 1: Go to this website and try out the Blackjack game:
+# https://games.washingtonpost.com/games/blackjack/
+#Then try out the completed Blackjack project here:
+# https://blackjack-final.appbrewery.repl.run 1
+#Hint 2: Read this breakdown of program requirements:
+# http://listmoz.com/view/6h34DIpvIBFVR1ZfIVXF
+#Then try to create your own flowchart for the program.
+#Hint 3: Download and read this flow chart I've created:
+#  https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjMlqwSuyEk-rPnt
+#Hint 4: Create a deal_card() function that uses the List below to *returnx a random card.
+#11 is the Ace.
+#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
 def f_108_blackJack():
    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
    print(f"cards: {cards}")
