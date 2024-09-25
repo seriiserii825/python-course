ccbd5ccefbb0958ef16d772db9549df61dedcd6d
serii burduja <seriiburduja@gmail.com>
2024-09-25 18:26:19 +0300

    feat: 119

---
+++ lessons/f_119_guessTheNumber.py
@@ -0,0 +1,30 @@
+# Welcome to the Number Guessing Game!
+# I'm thinking of a number between 1 and 100.
+# Choose a difficulty. Type 'easy' or 'hard': easy
+# You have 1@ attempts remaining to guess the number.
+# Make a guess: 50
+# Too high.
+# Guess again.
+# You have 9 attempts
+# Make a guess: 25
+# Too high.
+# Guess again.
+# You have 8 attempts
+# Make a guess: 10
+# Too low.
+# Guess again.
+# You have 7 attempts
+# Make a guess: 15
+# Too high.
+# Guess again.
+# You have 6 attempts
+# Make a guess: 12
+# remaining to guess the number.
+# remaining to guess the number.
+# &
+# remaining to guess the number.
+# remaining to guess the number.
+# This repl has exited, ?
+
+def f_119_guessTheNumber():
+    pass
