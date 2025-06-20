4b0a72a7b3001f7bcd96ecbddb546419d7273b5b
serii burduja <seriiburduja@gmail.com>
2025-06-20 21:46:48 +0300

    feat: l01 set

---
+++ lessons/l_01_set.py
@@ -0,0 +1,4 @@
+def l_01_set():
+    # set is unordered, unindexed, and does not allow duplicate values
+    post_ids = {123, 123, 456, 789, 123}
+    print(f"post_ids: {post_ids}")  # {123, 456, 789}
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from lessons.l_01_set import l_01_set
 from lessons.l_01_zip_func import l_01_zip_func
 
 
 def mainFunc():
-    l_01_zip_func()
+    # l_01_zip_func()
+    l_01_set()
 
 
 if __name__ == "__main__":
