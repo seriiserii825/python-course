3c30852b4305cff55baf8772fb8b4e482333cc48
serii burduja <seriiburduja@gmail.com>
2025-06-23 18:40:25 +0300

    feat: 7 extract list

---
+++ lessons/l_07_extract_list_tuple.py
@@ -0,0 +1,23 @@
+def l_07_extract_list_tuple():
+    my_list = [1, 2, 3, 4, 5]
+    first, second, third, fourth, fifth = my_list
+    print(f"First: {first}, Second: {second}, Third: {third}, Fourth: {fourth}, Fifth: {fifth}")
+
+    my_tuple = (10, 20, 30, 40, 50)
+    first_tuple, second_tuple, *rest_of_tuple = my_tuple
+    print(f'{first_tuple}: first_tuple')
+    print(f'{second_tuple}: second_tuple')
+    print(f'{rest_of_tuple}: rest_of_tuple')
+
+    user_profile = {
+            "name": "Alice",
+            'comments_qty': 5,
+            }
+
+    def user_info(name, comments_qty = 0):
+        if not comments_qty:
+            return f"User {name} has no comments."
+        return f"User {name} has {comments_qty} comments."
+
+    # print(user_info(user_profile['name'], user_profile['comments_qty']))
+    print(user_info(**user_profile))
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from lessons.l_01_set import l_01_set
 from lessons.l_02_zip_func import l_02_zip_func
 from lessons.l_05_extract_dict import l_05_extract_dict
+from lessons.l_07_extract_list_tuple import l_07_extract_list_tuple
 
 
 def mainFunc():
     # l_01_set()
     # l_02_zip_func()
-    l_05_extract_dict()
+    # l_05_extract_dict()
+    l_07_extract_list_tuple()
 
 
 if __name__ == "__main__":
