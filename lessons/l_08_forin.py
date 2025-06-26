03632b94ec0a7f39b9b193b959e3d240fb068931
serii <seriiburduja@gmail.com>
2025-06-26 07:36:16 +0300

    feat: enumerate

---
+++ lessons/l_08_forin.py
@@ -0,0 +1,17 @@
+def l_08_forin():
+    my_dict = {
+        "name": "Alice",
+        "age": 30,
+    }
+
+    # for item in my_dict.items():
+    #     key, value = item
+    #     print(f"{key}: {value}")
+
+    for key, value in my_dict.items():
+        print(f"{key}: {value}")
+
+    my_list = ['apple', 'banana', 'cherry']
+
+    for index, value in enumerate(my_list):
+        print(f"Index: {index}, Value: {value}")
--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from lessons.l_01_set import l_01_set
 from lessons.l_02_zip_func import l_02_zip_func
 from lessons.l_05_extract_dict import l_05_extract_dict
 from lessons.l_07_extract_list_tuple import l_07_extract_list_tuple
+from lessons.l_08_forin import l_08_forin
 
 
 def mainFunc():
     # l_01_set()
     # l_02_zip_func()
     # l_05_extract_dict()
-    l_07_extract_list_tuple()
+    # l_07_extract_list_tuple()
+    l_08_forin()
 
 
 if __name__ == "__main__":
